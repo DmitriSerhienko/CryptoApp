a30c7140d59feca4640e047fd064be90692cf2fd
Dimas <dvserhienko.pb@gmail.com>
2023-01-28 01:20:44 +0200

    update

--- app/src/main/java/com/example/cryptoapp/presentation/CoinDetailActivity.kt
+++ app/src/main/java/com/example/cryptoapp/presentation/CoinDetailActivity.kt
@@ -4,10 +4,8 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
 import com.example.cryptoapp.R
 import com.example.cryptoapp.databinding.ActivityCoinDetailBinding
-import com.squareup.picasso.Picasso
 
 class CoinDetailActivity : AppCompatActivity() {
 
@@ -23,14 +21,12 @@ class CoinDetailActivity : AppCompatActivity() {
             return
         }
         val fromSymbol = intent.getStringExtra(EXTRA_FROM_SYMBOL) ?: EMPTY_SYMBOL
-        if(savedInstanceState == null){
+        if (savedInstanceState == null) {
             supportFragmentManager
                 .beginTransaction()
-                .replace(R.id.fragment_container,CoinDetailFragment.newInstance(fromSymbol))
+                .replace(R.id.fragment_container, CoinDetailFragment.newInstance(fromSymbol))
                 .commit()
-
         }
-
     }
 
     companion object {
